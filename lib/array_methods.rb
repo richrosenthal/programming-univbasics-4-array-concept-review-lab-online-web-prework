1c47ad36acfcfe37bca091a49b454d56a8b1ca5f
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-10-28 06:03:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,10 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0 
+  value_to_find = 3
+  while counter < array.length do
+    if array[counter] == value_to_find
+      return array[counter]
+    else counter += 1
 end
 
 def find_max_value(array)
@@ -9,3 +14,5 @@ end
 def find_min_value(array)
   # Add your solution here
 end
+
+
